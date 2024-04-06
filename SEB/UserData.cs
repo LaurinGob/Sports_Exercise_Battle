4964fbfa8fd7e9bd9319ed376d5cb3d672a32703
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2024-04-06 22:20:27 +0200

    Create UserData.cs

---
+++ SEB/UserData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports_Exercise_Battle.SEB
+{
+    public class UserData
+    {
+        public string Name { get; set; } = "";
+        public string Bio { get; set; } = "";
+        public string Image { get; set; } = "";
+    }
+}
