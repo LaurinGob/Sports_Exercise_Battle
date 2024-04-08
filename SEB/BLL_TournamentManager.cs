d2947ac5e47f9b2d4a3abe6d4b24e71905352648
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2024-04-08 23:47:53 +0200

    TournamentManager Structure

--- SEB/BLL_TournamentManager.cs
+++ SEB/BLL_TournamentManager.cs
@@ -14,5 +14,27 @@ namespace Sports_Exercise_Battle.SEB
         private BLL_TournamentManager() { }
 
         public static BLL_TournamentManager Instance => lazyInstance.Value;
+
+        List<Tournament> tournaments;
+
+        public void NewTournamentEntry(TournamentEntry entry)
+        {
+            // adds new entry to tournament
+            Tournament currentTournament = GetTournament();
+            currentTournament.AddEntry(entry);
+        }
+
+        private Tournament GetTournament()
+        {
+            // returns running tournament or creates new one
+            foreach (Tournament joust in tournaments)
+            {
+                if (joust.active) { return joust; }
+            }
+            Tournament newTournament = new Tournament();
+            tournaments.Add(newTournament);
+            Console.WriteLine("New Tournament started!");
+            return newTournament;
+        }
     }
 }
--- SEB/Tournament.cs
+++ SEB/Tournament.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Sports_Exercise_Battle.SEB
 {
-    internal class Tournament
+    public class Tournament
     {
+        public bool active { get; set; } = true;
+        List<TournamentEntry> entries;
+
+        public Tournament() 
+        {
+            // starts timer that deactivates tournament after 2 minutes
+            Timer activeTimer = new Timer(SetInactive, null, 1000 * 60 * 2, Timeout.Infinite);
+        }
+
+        public void AddEntry(TournamentEntry entry)
+        {
+            entries.Add(entry);
+        }
+
+        // set inactive via timer callback
+        private void SetInactive(object state) { active = false; }
     }
 }
