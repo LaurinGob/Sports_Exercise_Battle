c85107106074dbcab3101ef567e45a36e64e8e53
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2024-04-08 22:40:23 +0200

    Objects used in Tournament

---
+++ SEB/BLL_TournamentManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports_Exercise_Battle.SEB
+{
+    public class BLL_TournamentManager
+    {
+        private static readonly Lazy<BLL_TournamentManager> lazyInstance = new Lazy<BLL_TournamentManager>(() => new BLL_TournamentManager());
+
+        // Private constructor to prevent instantiation
+        private BLL_TournamentManager() { }
+
+        public static BLL_TournamentManager Instance => lazyInstance.Value;
+    }
+}
