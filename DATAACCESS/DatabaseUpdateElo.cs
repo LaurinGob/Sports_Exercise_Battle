e8422a86adb55ae7e7a64fcf9aa2991b7b0c062e
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2024-04-10 21:05:50 +0200

    Create DatabaseUpdateElo.cs

---
+++ DATAACCESS/DatabaseUpdateElo.cs
@@ -0,0 +1,40 @@
+﻿using Npgsql;
+using Sports_Exercise_Battle.SEB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports_Exercise_Battle.DATAACCESS
+{
+    public class DatabaseUpdateElo : BCDatabaseQuery
+    {
+        public DatabaseUpdateElo(int userelo, string username) : base()
+        {
+            string queryString = "UPDATE users SET userELO = @userELO WHERE username = @username";
+
+            using (var cmd = new NpgsqlCommand(queryString, this.conn))
+            {
+                // Add parameters to the query
+                // SET
+                cmd.Parameters.AddWithValue("userELO", userelo);
+                // WHERE
+                cmd.Parameters.AddWithValue("username", username);
+
+                try
+                {
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    Console.WriteLine($"{rowsAffected} row(s) inserted successfully.");
+
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error in DatabaseUpdateElo: " + ex.Message);
+                    throw;
+                }
+            }
+        }
+    }
+}
