bfdabfba73781db96294c39f3323bff6987e30de
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2024-04-10 17:33:28 +0200

    Changed Tournament functionality

---
+++ SEB/Participant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports_Exercise_Battle.SEB
+{
+    public class Participant
+    {
+        public string ProfileName { get; set; }
+        public int Sum { get; set; }
+
+        public Participant(string profileName, int sum)
+        {
+            ProfileName = profileName;
+            Sum = sum;
+        }
+
+    }
+}
--- SEB/Tournament.cs
+++ SEB/Tournament.cs
@@ -10,7 +10,8 @@ namespace Sports_Exercise_Battle.SEB
     {
         public bool Active { get; private set; } = true;
         public DateTime TournamentStarted { get; private set; } = DateTime.Now;
-        public string FirstPlace { get; private set; } // the first in line
+        public string FirstPlace { get; private set; }
+        public List<Participant> Participants { get; private set; } = new List<Participant>();
         public List<TournamentEntry> entries { get; private set; } = new List<TournamentEntry>();
         
         public Tournament() 
@@ -23,18 +24,46 @@ namespace Sports_Exercise_Battle.SEB
         public void AddEntry(TournamentEntry entry)
         {
             entries.Add(entry);
-            SortByWinner(); // TODO: not working properly
+
+            // add participant to list of participant if not yet added and enters entry into tournament
+            // since the instance of the participant object is not known this is the only way to do this
+
+            // if the participant is found, add the entry.count directly to sum
+            bool firstEntry = true;
+            foreach (Participant participant in Participants)
+            {
+                if (participant.ProfileName == entry.ProfileName) 
+                { 
+                    firstEntry = false; 
+                    participant.Sum += entry.Count; 
+                }
+            }
+            if (firstEntry) 
+            { 
+                Participants.Add(new Participant(entry.ProfileName, entry.Count));
+            }
+            
+            SortByWinner();
         }
 
         // set inactive via timer callback
-        public void SetInactive(object state) { Active = false; } // TODO: change elo values
+        public void SetInactive(object state) { Active = false; ConcludeTournament(); }
+
+        private void ConcludeTournament()
+        {
+            // remove winner from participants
+            // Participants.Remove(FirstPlace); // not good weil history verfälscht
+            // reduce ELO of all other -1
+            // code
+            // increase ELO of winner +2
+            // code
+        }
 
         private void SortByWinner()
         {
-            // TODO: not working properly
             // Sorts list based on highest count and adds first in line to firstplace 
-            entries = entries.OrderBy(obj => obj.Count).ToList();
-            FirstPlace = entries.First().ProfileName;
+            Participants = Participants.OrderByDescending(obj => obj.Sum).ToList();
+            FirstPlace = Participants.First().ProfileName;
         }
     }
 }
