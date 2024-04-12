ddeaf5e654b8c37318a93c2de244f8418c426d82
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2024-04-13 01:38:17 +0200

    Added Log functionality

--- SEB/Tournament.cs
+++ SEB/Tournament.cs
@@ -15,12 +15,13 @@ namespace Sports_Exercise_Battle.SEB
         public string FirstPlace { get; private set; }
         public List<Participant> Participants { get; private set; } = new List<Participant>();
         public List<TournamentEntry> entries { get; private set; } = new List<TournamentEntry>();
-        
+        public List<String> Log { get; private set; } = new List<String>();
+
         public Tournament(int time) 
         {
             // starts timer that deactivates tournament after *time* seconds
             Timer activeTimer = new Timer(SetInactive, null, 1000 * time, Timeout.Infinite);
-            Console.WriteLine("New Tournament started!");
+            Log.Add(DateTime.UtcNow + ": Tournament started!");
         }
 
         public void AddEntry(TournamentEntry entry, int userID)
@@ -28,6 +29,7 @@ namespace Sports_Exercise_Battle.SEB
             // using session manager to get the current elo
             BLL_SessionManager SessionManager = BLL_SessionManager.Instance;
             entries.Add(entry);
+            
 
             // add participant to list of participant if not yet added and enters entry into tournament
             // since the instance of the participant object is not known this is the only way to do this
@@ -39,15 +41,22 @@ namespace Sports_Exercise_Battle.SEB
                 if (participant.ProfileName == entry.ProfileName) 
                 { 
                     firstEntry = false; 
-                    participant.Sum += entry.Count; 
+                    participant.Sum += entry.Count;
+                    // enter log for new entry
+                    Log.Add(DateTime.UtcNow.ToString("g") + ": New entry by " + entry.ProfileName + ": " + entry.Count + " " + entry.Name + " for a total sum of " + participant.Sum);
                 }
             }
             if (firstEntry) 
             { 
                 Participants.Add(new Participant(entry.ProfileName, entry.Count, SessionManager.GetELO(userID), userID));
+                // enter log for new participant
+                Log.Add(DateTime.UtcNow.ToString("g") + ": " + entry.ProfileName + " has entered the tournament with: " + entry.Count + " " + entry.Name + "/s");
             }
-            
+
+            string tempFirstplace = FirstPlace;
             SortByWinner();
+            // enter log for change in first place
+            if (tempFirstplace != FirstPlace) { Log.Add(DateTime.UtcNow.ToString("g") + ": " + entry.ProfileName + " pulls ahead with an entry of: " + entry.Count); }
         }
 
         // set inactive via timer callback
@@ -80,6 +89,7 @@ namespace Sports_Exercise_Battle.SEB
             // iterate over participants update winner with +2 all others with minus 1
             if (numberOfWinners == 1)
             {
+                Log.Add(DateTime.UtcNow.ToString("g") + ": Tournament concluded with " + FirstPlace + " as the Winner!");
                 Console.WriteLine("Tournament concluded with " + FirstPlace + " as the Winner!");
                 foreach (Participant participant in Participants)
                 {
@@ -87,14 +97,17 @@ namespace Sports_Exercise_Battle.SEB
                     if (participant.ProfileName == FirstPlace)
                     {
                         participant.Elo += 2;
+                        Log.Add(DateTime.UtcNow.ToString("g") + ": " + participant.ProfileName + " was awarded with 2 ELO");
                     }
                     else
                     {
                         participant.Elo -= 1;
+                        Log.Add(DateTime.UtcNow.ToString("g") + ": " + participant.ProfileName + " was punished with -1 ELO");
                     }
                 }
             } else
             {
+                Log.Add(DateTime.UtcNow.ToString("g") + ": Tournament concluded with multiple Winners!");
                 Console.WriteLine("Tournament concluded with multiple Winners!");
                 Console.WriteLine("Winners: ");
                 // incase of draws (works also with more than 2 winners!)
@@ -103,6 +116,7 @@ namespace Sports_Exercise_Battle.SEB
                     if (numberOfWinners > 0)
                     {
                         Participants[i].Elo += 1;
+                        Log.Add(DateTime.UtcNow.ToString("g") + ": " + Participants[i].ProfileName + " was awarded with 1 ELO");
                         numberOfWinners--;
                         if (numberOfWinners > 0)
                         {
@@ -118,8 +132,10 @@ namespace Sports_Exercise_Battle.SEB
                     } else
                     {
                         Participants[i].Elo -= 1;
+                        Log.Add(DateTime.UtcNow.ToString("g") + ": " + Participants[i].ProfileName + " was punished with -1 ELO");
                     }
                 }
+                Log.Add(DateTime.UtcNow.ToString("g") + ": Winners: " + tournamentRecord.Winner);
             }
 
             // insert elo in db
